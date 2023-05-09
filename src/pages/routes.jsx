fdb2dfc0becdf36d926a20e4629f65a962d81232
VladStroi <vladislavstroy@gmail.com>
2023-05-09 22:52:37 +0300

    Add home page, currency page, clear todo

--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -2,14 +2,13 @@ import * as React from 'react';
 import { GroupsContainer } from './groups';
 import { CategoryContainer } from './category';
 import { ProductsContainer } from './products';
+import TypographyTheme from './home/home';
+import { CurrenciesTable } from './currency/currency-container';
 
 export const homePage = {
   path: '/',
   name: 'Home',
-  content: <div>
-    Welcome to StroiShop.
-    TODO: move to separate componen
-  </div>,
+  content: <TypographyTheme/>
 };
 
 export const routes = [
@@ -32,6 +31,6 @@ export const routes = [
   {
     name: 'Currency',
     path: '/currency',
-    content: <div>Not implemented</div>,
+    content: <CurrenciesTable />,
   },
 ];
--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const API_HOST = 'http://localhost:3010';
 export const api = {
   getProducts: (params = {}) => {
     return axios.get(`${API_HOST}/product`, {
-      // TODO: support server sorting?
+      
       params: params.pagination
         ? { pagination: JSON.stringify(params.pagination) }
         : {}
